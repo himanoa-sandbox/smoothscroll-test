746a233a7a5ac481864fb3aeef5e5583d4f8ad89
himanoa <matsunoappy@gmail.com>
2018-10-05 00:56:30 +0900

    implement smoothscroll demo

--- index.js
+++ index.js
@@ -2,10 +2,28 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import smoothscroll from "smoothscroll-polyfill";
+
+smoothscroll.polyfill();
+
+const toTop = e => {
+  e.preventDefault();
+  window.scroll({ top: 0, left: 0, behavior: "smooth" });
+};
 const LoveNatori = () =>
-  new Array(10000)
-    .fill(
-      "名取すこだぞ名取すこだぞ名取すこだぞ名取すこだぞ名取すこだぞ名取すこだぞ名取すこだぞ名取すこだぞ名取すこだぞ"
-    )
-    .map(natori => <h1>natori</h1>);
-ReactDOM.render(<LoveNatori />, document.getElementById("app"));
+  new Array(100)
+    .fill("名取すこだぞ")
+    .map(natori => (
+      <h1>{`${Math.floor(
+        Math.random() * Math.floor(100)
+      ).toString()}: ${natori}`}</h1>
+    ));
+
+const App = () => (
+  <div>
+    <h1>🚑🍆🏍</h1>
+    <LoveNatori />
+    <button onClick={toTop}>病棟に戻ろう!</button>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
