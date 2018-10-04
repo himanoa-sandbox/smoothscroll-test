aa7b3e815111124de3db7c6052dfcb573a9a9736
himanoa <matsunoappy@gmail.com>
2018-10-05 00:35:14 +0900

    build now

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // 登録9万人おめでとうございます
+
+import React from "react";
+import ReactDOM from "react-dom";
+const LoveNatori = () =>
+  new Array(10000)
+    .fill(
+      "名取すこだぞ名取すこだぞ名取すこだぞ名取すこだぞ名取すこだぞ名取すこだぞ名取すこだぞ名取すこだぞ名取すこだぞ"
+    )
+    .map(natori => <h1>natori</h1>);
+ReactDOM.render(<LoveNatori />, document.getElementById("app"));
